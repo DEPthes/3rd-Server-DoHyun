f12363bfb76a1c96951157f9080a47304f1632b9
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-05-10 08:19:39 +0900

    파일이동

--- Spring-입문/src/main/java/hello/hellospring/domain/Member.java
+++ Spring-입문/src/main/java/hello/hellospring/domain/Member.java
@@ -1,6 +1,13 @@
 package hello.hellospring.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Member {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 자동으로 생성해주는 것을 아이덴티티라고 한다.
     private Long id; //데이터를 구분해주기 위해서 저장되는 id 값
     private String name;
 
